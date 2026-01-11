30e5854d8e8ee9981cceabc5a2847c773612f087
mohdaqdas-official <628443akdas@gmail.com>
2026-01-11 13:35:26 +0530

    fix railway mysql connection

--- db.js
+++ db.js
@@ -1,18 +1,21 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
+db.getConnection((err) => {
   if (err) {
-    console.log("DB Error:", err);
+    console.error("❌ MySQL Connection Failed:", err);
   } else {
-    console.log("MySql Connected Successfully.....");
+    console.log("✅ MySQL Connected to Railway");
   }
 });
 
